dc800eebe2357d479f5febb0b6d93d21844d20af
Yockomo <Funohs@yandex.ru>
2022-10-02 17:41:10 +0300

    same

--- Assets/Scripts/FIeld of VIew/CutoutObject.cs
+++ Assets/Scripts/FIeld of VIew/CutoutObject.cs
@@ -43,34 +43,4 @@ public class CutoutObject : MonoBehaviour
             }
         }
     }
-    
-    // private void GetCutoutObjects()
-    // {
-    //     Vector2 offset = targetObject.position - transform.position;
-    //     Physics.RaycastNonAlloc(transform.position, offset, hitObjects, offset.magnitude,wallMask);
-    // }
-    //
-    // private void Cutout()
-    // {
-    //     var cutoutPosition = GetCutoutPosition();
-    //     
-    //     for (int i = 0; i < hitObjects.Length; ++i)
-    //     {
-    //         Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
-    //
-    //         for (int j = 0; j < materials.Length; ++j)
-    //         {
-    //             materials[j].SetVector(CutoutPosition,cutoutPosition);
-    //             materials[j].SetFloat(CutoutSize, cutoutSize);
-    //             materials[j].SetFloat(FalloffSize, falloffSize);
-    //         }
-    //     }
-    // }
-    
-    private Vector3 GetCutoutPosition()
-    {
-        var cutoutPosition = mainCamera.WorldToViewportPoint(targetObject.transform.position);
-        cutoutPosition.y /= (Screen.width / Screen.height);
-        return cutoutPosition;
-    }
 }
